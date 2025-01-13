bc95fd1b5defaddccdf3890868ab2b8048974c7b
Victor Gomes Mendes <victorgomesmendes05@gmail.com>
2025-01-13 00:10:19 +0000

    finalizada a playlist tutorial

--- playground/1st-release-vgm/public/keyboard-listener.js
+++ playground/1st-release-vgm/public/keyboard-listener.js
@@ -1,13 +1,18 @@
 export default function createKeyboardListener(document) {
     const state = {
-        observers: []
+        observers: [],
+        playerId: null
+    }
+
+    function registerPlayerId(playerId) {
+        state.playerId = playerId
     }
 
     function subscribe(observerFunction) {
         state.observers.push(observerFunction)
     }
 
-    function notifyAllObservers(command) {
+    function notifyAll(command) {
         for (const observerFunction of state.observers) {
             observerFunction(command)
         }
@@ -19,14 +24,16 @@ export default function createKeyboardListener(document) {
         const keyPressed = event.key
 
         const command = {
-            playerId: 'player1',
+            type: 'move-player',
+            playerId: state.playerId,
             keyPressed
         }
 
-        notifyAllObservers(command)
+        notifyAll(command)
     }
 
     return {
-        subscribe
+        subscribe,
+        registerPlayerId
     }
 }
